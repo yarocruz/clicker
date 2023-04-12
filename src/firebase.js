7bc8ab8f89c01ae2a380e0551ccf31799150b255
yarocruz <yarocruz@gmail.com>
2023-04-12 14:59:23 -0400

    removed unused imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, updateDoc, addDoc, query, where } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, updateDoc, addDoc } from 'firebase/firestore/lite';
 // Follow this pattern to import other Firebase services
 // import { } from 'firebase/<service>';
 
