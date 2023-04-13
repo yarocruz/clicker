f107b2cf81a232df03340602747a61176076a12e
yarocruz <yarocruz@gmail.com>
2023-04-13 11:36:58 -0400

    fixing bad logic

--- src/firebase.js
+++ src/firebase.js
@@ -38,7 +38,6 @@ export async function updateClicks(country, city) {
             const unknown = querySnapshot.docs.find(doc =>
                 (doc.data().name === 'unknown' && doc.data().city === 'unknown'));
             await updateDoc(unknown.ref, { clicks: unknown.data().clicks + 1 });
-            return getData();
         }
         await addDoc(countriesRef, { name: country, city: city, clicks: 1 });
     }
